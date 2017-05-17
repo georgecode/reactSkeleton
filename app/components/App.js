051a126e5233c16dd75b2567a199c73f19acc154
georgecode <georgeczurlanis@gmail.com>
2017-05-16 18:24:22 -0700

    Add es6 class update

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,22 @@
 var React = require('react');
 
-module.exports = React.createClass({
-  render: function() {
-    return (
-      // <h1>Hello React</h1>
-      <div className="helloContainer">
-          <h1 className="hello">Hello React</h1>
-          <h2>TWO plus TWO equals {2+2}</h2>
-      </div>
-    )
-  }
-});
+class Test extends React.Component{
+	render(){
+		return(
+			<div>
+			<h1>zzzHELLO</h1>
+			</div>
+			)
+	}
+}
+
+
+module.exports = class extends React.Component{
+	render(){
+		return(
+			<div>
+				<Test />
+			</div>
+			)
+	}
+}
